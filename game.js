fdda9eeeef678f066ae6c3a1936ea38b1fceac93
AronBodzas <aron.bodzas42@gmail.com>
2022-01-10 14:40:45 +0100

    chore: skeleton for the js file

--- game.js
+++ game.js
@@ -5,3 +5,70 @@ function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+
+function gameLoop() {
+
+}
+
+function initSnake() {
+
+}
+
+function snakeMovment() {
+
+}
+
+function snakeGrow() {
+
+}
+
+function snakeDeath() {
+
+}
+
+function initBoard() {
+
+}
+
+function initFood() {
+
+}
+
+function removeFood() {
+
+}
+
+function isFood() {
+
+}
+
+function isWall() {
+
+}
+
+function isSnake() {
+
+}
+
+function gameOver() {
+
+}
+
+function snakeGoesFaster() {
+
+}
+
+//extra stuff
+
+function score() {
+
+}
+
+function foodDespawn() {
+
+}
+
+function invertAxis() {
+
+}
+
