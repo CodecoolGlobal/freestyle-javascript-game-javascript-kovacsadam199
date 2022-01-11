9e72b3cfd4f7ab85307ca2d8a9375f46da4d8acd
DianaDob <dobsinszki.diana@gmail.com>
2022-01-11 10:15:53 +0100

    feat: implement initBoard function

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const game = {
     initGame: function (){
+        this.initBoard();
         //TODO: the game setup goes here.
         this.gameLoop(); //Use function calls like this.
     },
@@ -19,8 +20,43 @@ const game = {
 
     },
     initBoard: function (){
+        const rows = 10;
+        const cols = 10;
 
+        let gameField = document.querySelector(".game-field");
+        this.setGameFieldSize(gameField, rows, cols);
+        let cellIndex = 0
+        for (let row = 0; row < rows; row++) {
+            const rowElement = this.addRow(gameField);
+            for (let col = 0; col < cols; col++) {
+                this.addCell(rowElement, row, col);
+                cellIndex++;
+
+            }
+        }
     },
+
+    setGameFieldSize: function (gameField, rows, cols) {
+        gameField.style.width = (gameField.dataset.cellWidth * rows) + 'px';
+        gameField.style.height = (gameField.dataset.cellHeight * cols) + 'px';
+    },
+
+    addRow: function (gameField) {
+        gameField.insertAdjacentHTML(
+            'beforeend',
+            '<div class="row"></div>'
+        );
+        return gameField.lastElementChild;
+    },
+
+    addCell: function (rowElement, row, col) {
+        rowElement.insertAdjacentHTML(
+            'beforeend',
+            `<div class="field"
+                        data-row="${row}"
+                        data-col="${col}"></div>`);
+    },
+
     initFood: function (){
 
     },
